4a85d2a3402a61f0b7626e749bd2b6e18aafab62
Shivam7400 <shivammaheshwari317@gmail.com>
2023-06-21 23:53:46 +0530

    Committing changes in expenseswebsite register

--- authentication/views.py
+++ authentication/views.py
@@ -3,8 +3,19 @@ from django.views import View
 import json
 from django.http import JsonResponse
 from django.contrib.auth.models import User
+from validate_email import validate_email 
 
-class UsernamevalidationView(View):
+class EmailValidationView(View):
+    def post(self, request):
+        data=json.loads(request.body)
+        email = data['email']
+        if not validate_email(email):
+            return JsonResponse({'email_error':'Email is invalid'}, status=400)
+        if User.objects.filter(email=email).exists():
+            return JsonResponse({'email_error':'sorry email is used, choose another email '}, status=409)
+        return JsonResponse({'email_valid': True})
+
+class UsernameValidationView(View):
     def post(self, request):
         data=json.loads(request.body)
         username = data['username']
@@ -12,8 +23,6 @@ class UsernamevalidationView(View):
             return JsonResponse({'username_error':'username should only contain alphanumeric characters...'}, status=400)
         if User.objects.filter(username=username).exists():
             return JsonResponse({'username_error':'sorry usernamein is used, choose another username '}, status=409)
-        
-
         return JsonResponse({'username_valid': True})
 
 class RegistrationView(View):
