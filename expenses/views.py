2f3b390c51e3be25008743fdf675b58ea32effa2
Shivam7400 <shivammaheshwari317@gmail.com>
2023-07-16 00:53:11 +0530

    Added export secion in different formet

--- expenses/views.py
+++ expenses/views.py
@@ -4,10 +4,9 @@ from .models import Category, Expense
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.core.paginator import Paginator
-import json
-from django.http import JsonResponse
+import json, datetime, csv, xlwt
+from django.http import JsonResponse, HttpResponse
 from userpreferences.models import UserPreference
-import datetime
 
 def search_expenses(request):
     if request.method == 'POST':
@@ -24,7 +23,7 @@ def search_expenses(request):
 def index(request):
     categories = Category.objects.all()
     expenses = Expense.objects.filter(owner=request.user)
-    paginator = Paginator(expenses, 2)
+    paginator = Paginator(expenses, 5)
     page_number = request.GET.get('page')
     page_obj = Paginator.get_page(paginator, page_number)
     try:
@@ -86,7 +85,7 @@ def expense_edit(request, id):
             return render(request, 'expenses/edit-expense.html', context)
         expense.owner = request.user
         expense.amount = amount
-        expense. date = date
+        expense.date = date
         expense.category = category
         expense.description = description
         expense.save()
@@ -98,3 +97,59 @@ def delete_expense(request, id):
     expense.delete()
     messages.success(request, 'Expense removed')
     return redirect('expenses')
+
+def expense_category_summary(request):
+    todays_date = datetime.date.today()
+    six_mounths_ago = todays_date - datetime.timedelta(days = 30 * 6)
+    expenses = Expense.objects.filter(owner = request.user, date__range = (six_mounths_ago, todays_date))
+    finalrep = {}
+
+    def get_category(expense):
+        return expense.category    
+    category_list = list(set(map(get_category, expenses)))
+
+    def get_expense_category_amount(category):
+        amount = 0
+        filtered_by_category = expenses.filter(category = category)
+        for item in filtered_by_category:
+            amount += item.amount
+        return amount    
+    for x in expenses:
+        for y in category_list:
+            finalrep[y] = get_expense_category_amount(y)
+
+    return JsonResponse({'expense_category_data' : finalrep}, safe=False)
+
+def stats_view(request):
+    return render(request, 'expenses/stats.html')
+
+def export_csv(request):
+    response = HttpResponse(content_type = 'text/csv')
+    response['Content-Disposition'] = 'attachment; filename = Expenses' + str(datetime.datetime.now()) +'.csv'
+    writer = csv.writer(response)
+    writer.writerow(['Amount', 'Description', 'Category', 'Date'])
+    expenses = Expense.objects.filter(owner=request.user)
+    for expense in expenses:
+        writer.writerow([expense.amount, expense.description, expense.category, expense.date])
+    return response
+
+def export_excel(request):
+    response = HttpResponse(content_type = 'application/ms-excel') 
+    response['Content-Disposition'] = 'attachment; filename = Expenses' + str(datetime.datetime.now()) +'.xls'
+    wb = xlwt.Workbook(encoding = 'utf-8')
+    ws = wb.add_sheet('Expenses')
+    row_num = 0
+    font_style = xlwt.XFStyle()
+    font_style.font.bold = True
+    columns = ['Amount', 'Description', 'Category', 'Date']
+    for col_num in range(len(columns)):
+        ws.write(row_num, col_num, columns[col_num], font_style)
+
+    font_style = xlwt.XFStyle()
+    rows = Expense.objects.filter(owner=request.user).values_list('amount', 'description', 'category', 'date')
+    for row in rows:
+        row_num+=1
+        for col_num in range(len(row)):
+            ws.write(row_num, col_num, str(row[col_num]), font_style)
+    wb.save(response)
+    return response
