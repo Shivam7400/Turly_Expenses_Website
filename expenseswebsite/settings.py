5e469461c682c8182a591895f4cca372f7d21cc7
Shivam7400 <shivammaheshwari317@gmail.com>
2023-07-20 06:29:56 +0530

    Added vercel setting

--- expenseswebsite/settings.py
+++ expenseswebsite/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-m7gmr))s&xuix&nzzpyki2)ffcjoe0h=5lc6$0+o+%efhc-z9r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
